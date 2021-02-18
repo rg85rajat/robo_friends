43af8f97c7d562a323ce4522b34322556cd63d50
rgrajat085 <rajatg62@gmail.com>
2021-02-18 23:48:50 +0530

    robofriends

---
+++ src/containers/App.js
@@ -0,0 +1,61 @@
+import React,{Component} from 'react';
+import CardList from '../components/CardList'; 
+// import {robots} from './robots';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+class App extends Component{
+	constructor()
+	{
+		super()
+		this.state=
+		{
+		  // robots: robots,
+		  //remove componentDidMount to use commented part
+		  robots:[],
+	      searchfield:''
+
+	}
+}
+componentDidMount()
+{
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response=> response.json())
+	.then(users=>this.setState({robots:users}))
+	//if users is not returned it will show loading.(in render method/ or it taked more then 0 sec)
+	//users: jsonplaceholderfile
+}
+onSearchChange=(event)=>
+{
+	this.setState({searchfield:event.target.value})
+	
+}
+render()
+{
+	const {robots,searchfield}=this.state;
+	const filteredRobots=robots.filter(robot=>
+	{
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+	})
+	if (!robots.length) //similar to === 0
+	{
+		return <h1> Loading </h1>
+	}
+	else{
+ return(
+   	<div className='tc'>
+  	<h1 className='f1'>RoboFriends </h1>
+  	<SearchBox searchChange={this.onSearchChange}/> 
+    <Scroll>
+    <ErrorBoundry>
+    <CardList robots={filteredRobots}/>
+    </ErrorBoundry>
+    </Scroll>
+    </div>
+  );
+}
+}
+}
+export default App;
